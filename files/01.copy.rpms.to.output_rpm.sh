6f2534a6516c2807d7549c9f20b74bc068701f1b
Leif Liddy <leif.liddy@gmail.com>
2022-02-08 17:10:24 +0100

    resolved patch issue

--- files/01.copy.rpms.to.output_rpm.sh
+++ files/01.copy.rpms.to.output_rpm.sh
@@ -4,7 +4,7 @@ find /output_rpm/ -type f | grep '\.rpm$' | xargs rm -f
 find /root/rpmbuild/RPMS/ -type f | grep '\.rpm$' | xargs cp -t /output_rpm/
 
 # copy SRPM
-#find /root/rpmbuild/SRPMS/ | grep '\.rpm$' | xargs cp -t /output_rpm/
+find /root/rpmbuild/SRPMS/ | grep '\.rpm$' | xargs cp -t /output_rpm/
 
 echo -e "\ncontents of /output_rpm"
 ls -lA /output_rpm | grep -v .gitignore
